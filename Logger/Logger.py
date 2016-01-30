7bd577da90eafbc8823caa5ac24bce98a48bafee
wixenius <fredrik.wixenius@hotmail.com>
2016-01-30 20:18:07 +0100

    Delete Logger.py

--- Logger/Logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class Logger:
-
-    def __init__(self):
-        logging.basicConfig(filename='example.log', level=logging.DEBUG, format='%(levelname)s %(asctime)s : %(message)s')
-
-    def log(self, stringToLog):
-        logging.error(stringToLog)
